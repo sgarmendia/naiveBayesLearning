4f26e3d9373c0553f27747d46da3dcd8445b516d
Simon Garmendia <sgarmendia@gmail.com>
2017-07-25 23:15:34 +0200

    finished learning module v4.0

--- server/routes/handlers/teach.js
+++ server/routes/handlers/teach.js
@@ -8,12 +8,29 @@ function teachSentiment(req, res) {
 
     let json = JSON.parse(fs.readFileSync('./learning/learnJson.json', 'utf8'))
 
-    json[1].mood +=1
-    const sText = teachtext.split('')
+    json[1][mood] += 1
+    const aText = teachtext.split(' ')
+    let oMood = json[2][mood]
+    let sumMood = 0
 
+    aText.forEach(word => {
+
+        oMood[word] ? oMood[word] += 1 : oMood[word] = 1
+
+        if (json[4].indexOf(word) < 0) json[4].push(word)
+
+    })
+
+    for (let key in oMood) {
+
+        sumMood += oMood[key] 
+
+    }
+
+    json[3][mood] = sumMood
 
     const sJson = JSON.stringify(json)
-    fs.writeFile ('./learning/newJson.json', sJson , err => {if (err) throw err} )
+    fs.writeFile('./learning/newJson.json', sJson, err => { if (err) throw err })
 
     res.send(sJson)
 
