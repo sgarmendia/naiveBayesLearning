e88962d7bb5e92ca24094645117a29f03fcaa366
Simon Garmendia <sgarmendia@gmail.com>
2017-07-29 01:20:20 +0200

    update bootstrap & bower

--- client/js/controllers/predictController.js
+++ client/js/controllers/predictController.js
@@ -1,13 +1,21 @@
 angular.module('bayesApp')
-    .controller('predictController', function($scope, machineLearningService) {
+  .controller('predictController', function($scope, machineLearningService) {
 
-        $scope.predict = predicttext => {
-            machineLearningService.predictSentiment({ predicttext })
-                .then(response => {
-                    console.log(response.data)
-                    $scope.prediction = response.data
-                })
-            $scope.predicttext = ''
-            document.getElementById('sampletext').focus()
-        }
-    })
+    $scope.predict = predicttext => {
+      machineLearningService.predictSentiment({
+          predicttext
+        })
+        .then(response => {
+
+          console.log(response.data)
+          $scope.prediction = response.data
+          const ratioProb = Math.pow(2, response.data.odds[0][1])
+          $scope.probability = +(((ratioProb / (1 + ratioProb)) * 100).toFixed(2))
+
+        })
+
+      $scope.predicttext = ''
+      document.getElementById('sampletext').focus()
+
+    }
+  })
